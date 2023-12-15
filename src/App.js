65dd5fa8c7ac0ba6b6765d9b490ab530abc37d1b
lazyjinn1 <joshjbarba@gmail.com>
2023-12-14 16:15:57 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to the Meet App</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
