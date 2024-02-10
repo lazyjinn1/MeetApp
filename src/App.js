f73beff5741b5ea49b81f61baefc13593b7437ea
lazyjinn1 <joshjbarba@gmail.com>
2024-02-09 17:02:18 -0800

    Submitting for 4.10
    Added Charts and played around with some CSS

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 import './App.css';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -64,10 +65,16 @@ const App = () => {
         onNumberOfEventsChange={handleNumberOfEventsChange}
         setErrorAlert={setErrorAlert}
       />
-      <CityEventsChart
-        allLocations={allLocations}
-        events={events}
-      />
+      <div className='charts-container'>
+        <EventGenresChart
+          events={events}
+        />
+        <CityEventsChart
+          allLocations={allLocations}
+          events={events}
+        />
+      </div>
+
       <EventList events={events} />
     </div>
   );
--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -12,7 +12,7 @@ const CityEventsChart = ({ allLocations, events }) => {
     const getData = () => {
         const data = allLocations.map((location) => {
             const count = events.filter((event) => event.location === location).length
-            const city = location.split(', ')[0]
+            const city = location.split(', | - ')[0]
             return { city, count };
         })
         return data;
@@ -24,16 +24,16 @@ const CityEventsChart = ({ allLocations, events }) => {
                 margin={{
                     top: 20,
                     right: 20,
-                    bottom: 20,
-                    left: 20,
+                    bottom: 60,
+                    left: -20,
                 }}
             >
                 <CartesianGrid verticalFill={['black']} fillOpacity={.4} stroke='black' />
                 <XAxis
-                    type="category" dataKey="city" name="City"
+                    type="category" dataKey="city" name="City" stroke ='black' strokeWidth={2}  
                     angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
                 />
-                <YAxis type='number' dataKey='count' name='Number of Events' />
+                <YAxis type='number' dataKey='count' name='Number of Events' stroke ='black' strokeWidth={2}  />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                 <Scatter name='A school' data={data} fill='red' />
             </ScatterChart>
